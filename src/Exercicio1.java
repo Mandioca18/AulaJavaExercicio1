af521b42ebe0be8650c510fbfb82989f26f82d2a
ManiocDev18 <renan.guimamendonca@gmail.com>
2023-09-13 16:20:50 -0300

    Initial commit

---
+++ src/Exercicio1.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Qual é o seu Nome Bagual?!!");
+        String name = scan.next();
+
+        System.out.println("Tri Legal!! Gostei de saber que seu nome é " + name);
+    }
+}
